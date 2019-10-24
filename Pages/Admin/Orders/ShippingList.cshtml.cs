4e79eb36866cbe1bfa1f798d4161b51de9482161
Ole Kristian Losvik <losvik+github@gmail.com>
2019-10-24 20:12:43 +0200

    Added shippinglist

---
+++ Pages/Admin/Orders/ShippingList.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Shoppur.Data;
+using Shoppur.Models;
+using static Shoppur.Models.Order;
+
+namespace Shoppur.Pages.Admin.Orders
+{
+    public class ShippingListModel : PageModel
+    {
+        private readonly Shoppur.Data.ApplicationDbContext _context;
+
+        public ShippingListModel(Shoppur.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Order> Orders { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Orders = await _context.Orders
+                .Include(i => i.OrderLines)
+                .Where(i => i.Status == OrderStatus.Paid)
+                .ToListAsync();
+        }
+    }
+}
