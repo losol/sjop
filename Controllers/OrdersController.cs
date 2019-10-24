55b3af5ebd2fa9aadcdb05f85cf7818dedfc242d
Ole Kristian Losvik <losvik+github@gmail.com>
2019-10-24 23:26:13 +0200

    Formatted

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -16,147 +16,151 @@ using static Shoppur.Models.Order;
 
 namespace Shoppur.Controllers
 {
-    [Authorize(Policy = "RequireAdministratorRole")]
-    [Route("api/v1/orders")]
-    [ApiController]
-    public class OrdersController : ControllerBase
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly ILogger _logger;
+	[Authorize(Policy = "RequireAdministratorRole")]
+	[Route("api/v1/orders")]
+	[ApiController]
+	public class OrdersController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly ILogger _logger;
 
-        public OrdersController(ApplicationDbContext context, ILogger<OrdersController> logger)
-        {
-            _context = context;
-            _logger = logger;
-        }
+		public OrdersController(ApplicationDbContext context, ILogger<OrdersController> logger)
+		{
+			_context = context;
+			_logger = logger;
+		}
 
-        // POST: api/orders/register
-        [AllowAnonymous]
-        [HttpPost]
-        [Route("register")]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Order>> RegisterOrder([FromBody]OrderRegistrationVM orderRegistration)
-        {
-            _logger.LogInformation("*** Submit order from cart ***");
-            
-            var customer = new CustomerInfo {
-                Name = "asdf",
-                Email = "asdf@asdf.com"
-            };
+		// POST: api/orders/register
+		[AllowAnonymous]
+		[HttpPost]
+		[Route("register")]
+		[Consumes(MediaTypeNames.Application.Json)]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<ActionResult<Order>> RegisterOrder([FromBody]OrderRegistrationVM orderRegistration)
+		{
+			_logger.LogInformation("*** Submit order from cart ***");
 
-            var paymentprovider = PaymentProviderType.StripeCheckout;
+			var customer = new CustomerInfo
+			{
+				Name = "asdf",
+				Email = "asdf@asdf.com"
+			};
 
-            var order = new Order { 
-                Customer = customer, 
-                PaymentProvider = paymentprovider, 
-                Status = OrderStatus.Draft,
-                Shipping = ShippingStatus.Draft
-            };
+			var paymentprovider = PaymentProviderType.StripeCheckout;
 
-            await _context.Orders.AddAsync(order);
-            await _context.SaveChangesAsync();
+			var order = new Order
+			{
+				Customer = customer,
+				PaymentProvider = paymentprovider,
+				Status = OrderStatus.Draft,
+				Shipping = ShippingStatus.Draft
+			};
 
-            _logger.LogCritical("OrderId: " +order.Id);
-                      
-            foreach (var item in orderRegistration.Cart.CartItems) {
-                var line = new OrderLine {
-                    OrderId = order.Id,
-                    ProductId = item.ProductId,
-                    Quantity = 1
-                };
-                await _context.OrderLines.AddAsync(line);
-            }
-            await _context.SaveChangesAsync();
+			await _context.Orders.AddAsync(order);
+			await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
-        }
+			_logger.LogCritical("OrderId: " + order.Id);
 
-        // GET: api/orders
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Order>>> GetOrder()
-        {
-            return await _context.Orders.ToListAsync();
-        }
+			foreach (var item in orderRegistration.Cart.CartItems)
+			{
+				var line = new OrderLine
+				{
+					OrderId = order.Id,
+					ProductId = item.ProductId,
+					Quantity = 1
+				};
+				await _context.OrderLines.AddAsync(line);
+			}
+			await _context.SaveChangesAsync();
 
-        // GET: api/Orders/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrder(int id)
-        {
-            var order = await _context.Orders.FindAsync(id);
+			return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
+		}
 
-            if (order == null)
-            {
-                return NotFound();
-            }
+		// GET: api/orders
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Order>>> GetOrder()
+		{
+			return await _context.Orders.ToListAsync();
+		}
 
-            return order;
-        }
+		// GET: api/Orders/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Order>> GetOrder(int id)
+		{
+			var order = await _context.Orders.FindAsync(id);
 
-        // PUT: api/orders/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder(int id, Order order)
-        {
-            if (id != order.Id)
-            {
-                return BadRequest();
-            }
+			if (order == null)
+			{
+				return NotFound();
+			}
 
-            _context.Entry(order).State = EntityState.Modified;
+			return order;
+		}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+		// PUT: api/orders/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+		// more details see https://aka.ms/RazorPagesCRUD.
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutOrder(int id, Order order)
+		{
+			if (id != order.Id)
+			{
+				return BadRequest();
+			}
 
-            return NoContent();
-        }
+			_context.Entry(order).State = EntityState.Modified;
 
-        // POST: api/orders
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
-        {
-            _context.Orders.Add(order);
-            await _context.SaveChangesAsync();
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!OrderExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
-        }
+			return NoContent();
+		}
 
-        // DELETE: api/Orders/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Order>> DeleteOrder(int id)
-        {
-            var order = await _context.Orders.FindAsync(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
+		// POST: api/orders
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+		// more details see https://aka.ms/RazorPagesCRUD.
+		[HttpPost]
+		public async Task<ActionResult<Order>> PostOrder(Order order)
+		{
+			_context.Orders.Add(order);
+			await _context.SaveChangesAsync();
 
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
+			return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+		}
 
-            return order;
-        }
+		// DELETE: api/Orders/5
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Order>> DeleteOrder(int id)
+		{
+			var order = await _context.Orders.FindAsync(id);
+			if (order == null)
+			{
+				return NotFound();
+			}
 
-        private bool OrderExists(int id)
-        {
-            return _context.Orders.Any(e => e.Id == id);
-        }
-    }
+			_context.Orders.Remove(order);
+			await _context.SaveChangesAsync();
+
+			return order;
+		}
+
+		private bool OrderExists(int id)
+		{
+			return _context.Orders.Any(e => e.Id == id);
+		}
+	}
 }
